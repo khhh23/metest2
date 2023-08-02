aeb87880c801a54cd2bab487bcdbc941ec4168af
DSM <DSM@DESKTOP-O106SID>
2023-08-02 15:44:06 +0900

    test test54844

---
+++ src/main/test3.java
@@ -0,0 +1,8 @@
+package main;
+
+public class test3 {
+	public static void main(String[] args) {
+		// 11 222 3333 5555test3555
+	}
+}
+
---
+++ src/main/test5.java
@@ -0,0 +1,8 @@
+package main;
+
+public class test5 {
+	public static void main(String[] args) {
+		// 11 222 3333 5555 test5
+	}
+}
+
